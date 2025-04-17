dffcb86e70de9a05a316ea4f60d98d13dbe48dec
hvnmember <94003219+Ademma2222@users.noreply.github.com>
2025-04-17 23:27:36 +0300

    Update Ant.h

--- Ant.h
+++ Ant.h
@@ -1,58 +1,40 @@
 #pragma once
 #include "Role.h"
 #include "Informer.h"
-
 #include <cstdlib>
-
 class AntHill;
-
 class Ant
 {
 private:
     int vozrast;
     int zdorovie;
-    Role * rolya;
-    AntHill * muraveynik;
+    Role*rolya;
+    AntHill*muraveynik;
     int id;
     static int sled_id;
-  
-   
-    
-    void UpdateRole ();
-    
+    void UpdateRole();
 public:
-    Ant ();
-    ~Ant ();
-    
-    void Rosti ();
-    void Zhivi ();
+    Ant();
+    ~Ant();
+    void Rosti();
+    void Zhivi();
         // Обработка событий от информеров
-    void ReagirovatNaVragov ();
-    void PomochStroitelstvu ();
-    void PomochSboru ();
-    
+    void ReagirovatNaVragov();
+    void PomochStroitelstvu();
+    void PomochSboru();
         // Подписка и отписка на информеры
-    void PodpisatsyaNaInformer ( Informer * );
-    void OtpuditOtInformera ( Informer * );
-    
-    void PoluchitUron ( int uro );
-    bool EstLiZhizn () const;
-    
-    void UstanovitSvyaz ( AntHill * ukazatel );
+    void PodpisatsyaNaInformer (Informer*);
+    void OtpuditOtInformera (Informer*);
+    void PoluchitUron (int uro);
+    bool EstLiZhizn()const;
+    void UstanovitSvyaz (AntHill*ukazatel);
    ;
-    
-    int GetId () const;
-    std::string GetOpisanie () const;
-    
-    bool JavlyaetsyaSoldatom () const;
-    
-    Role * GetRole () const;
-    
-    void Vosstanovit ( int k );
-    
-    AntHill * GetMuraveynik () const;
-    
-   
+    int GetId()const;
+    std::string GetOpisanie()const;
+    bool JavlyaetsyaSoldatom()const;
+    Role*GetRole()const;
+    void Vosstanovit (int k);
+    AntHill*GetMuraveynik() const;
 };
 
 
