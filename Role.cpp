ef4ef45996dce9eb646ee8ab5ec9aaacb1b70aba
hvnmember <94003219+Ademma2222@users.noreply.github.com>
2025-04-17 23:25:24 +0300

    Update Role.cpp

--- Role.cpp
+++ Role.cpp
@@ -2,34 +2,30 @@
 #include "Ant.h"
 #include "AntHill.h"
 #include <iostream>
-
-void Nurse::Work ( Ant * kto )
+void Nurse::Work (Ant*kto)
 {
-    std::cout << kto->GetOpisanie () << " заботится о детях.\n";
+    std::cout << kto->GetOpisanie()<< " заботится о детях.\n";
  
 }
 
-void Soldier::Work ( Ant * kto )
+void Soldier::Work (Ant*kto)
 {
-    std::cout << kto->GetOpisanie () << " охраняет муравейник.\n";
+    std::cout << kto->GetOpisanie() << " охраняет муравейник.\n";
 }
-
-void Shepherd::Work ( Ant * kto )
+void Shepherd::Work (Ant*kto)
 {
-    std::cout << kto->GetOpisanie () << " присматривает за тлёй.\n";
+    std::cout << kto->GetOpisanie() << " присматривает за тлёй.\n";
 }
-
-void Forager::Work ( Ant * kto )
+void Forager::Work (Ant*kto)
 {
     std::cout << kto->GetOpisanie () << " ищет еду.\n";
 }
 
-void Builder::Work ( Ant * kto )
+void Builder::Work (Ant*kto)
 {
-    std::cout << kto->GetOpisanie () << " укрепляет муравейник.\n";
+    std::cout << kto->GetOpisanie() << " укрепляет муравейник.\n";
 }
-
-void Cleaner::Work ( Ant * kto )
+void Cleaner::Work (Ant*kto)
 {
-    std::cout << kto->GetOpisanie () << " чистит туннели.\n";
+    std::cout << kto->GetOpisanie() << " чистит туннели.\n";
 }
